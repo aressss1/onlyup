47a1342bc18990cc4df5349ceb33ef2e72270c85
aressss1 <codewithfaizi@gmail.com>
2024-02-08 10:58:57 +0500

    f-

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -13,13 +13,13 @@ const font = localFont({
 
 const HeroSection = () => {
     return (
-        <div className="relative" >
-            <div className="bg-[url('/bg-1.jpg')] absolute top-0 w-full object-cover " />
-            <div className="py-5 h-screen flex flex-col z-[10] justify-between gap-28 items-center "  >
+        <div className="relative h-screen " >
+            <div className="bg-[url('/bg-2.jpg')] absolute top-0 opacity-50  w-full object-cover " />
+            <div className="py-5  flex flex-col z-[10] justify-between gap-28 items-center "  >
                 <div className="flex flex-col items-center gap-5 my-auto pt-36 " >
                     <div className={cn(
                         font.className,
-                        "lg:text-6xl md:text-5xl  text-2xl lg:leading-[4rem] lg:w-[706px] md:w-[565px] w-[283px] leading-tight "
+                        "lg:text-6xl md:text-5xl  text-2xl lg:leading-[5rem] lg:w-[730px] md:w-[565px] w-[283px] leading-tight "
                     )}>
                         Turning Ideas into Artful Realities.
                     </div>
