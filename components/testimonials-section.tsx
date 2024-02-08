beac07c90e00222555382e95c6d7bed0413653ce
aressss1 <codewithfaizi@gmail.com>
2024-02-08 10:09:39 +0500

    f-

--- components/testimonials-section.tsx
+++ components/testimonials-section.tsx
@@ -18,26 +18,31 @@ const TestimonialsSection = () => {
         },
     ]
     return (
-        <div className=" px-[17rem] py-10 flex gap-6 " >
-            {reviews.map((review) => (
-                <div key={review.clientName} className="bg-white rounded-xl p-12" >
-                    <p className="text-2xl leading-6 " >
-                        {review.review} 
-                    </p>
-                    <div className="mt-6 flex" >
-                        <Image
-                            className="w-20 h-20 rounded-full object-cover"
-                            alt={review.clientName}
-                            src={review.clientImage}
-                        />
-                        <div className="flex flex-col pl-8 my-auto" >
-                            <div>{review.clientName}</div>
-                            <div>{review.clientCompanyName}</div>
-                            <div>{review.clientRole}</div>
+        <div className="flex flex-col gap-8" >
+            <div className="text-sm font-normal opacity-80 uppercase " >
+                Testimonials
+            </div>
+            <div className=" px-[17rem] py-10 flex gap-6 " >
+                {reviews.map((review) => (
+                    <div key={review.clientName} className="bg-white rounded-xl p-12" >
+                        <p className="text-2xl leading-6 " >
+                            {review.review}
+                        </p>
+                        <div className="mt-6 flex" >
+                            <Image
+                                className="w-20 h-20 rounded-full object-cover"
+                                alt={review.clientName}
+                                src={review.clientImage}
+                            />
+                            <div className="flex flex-col pl-8 my-auto" >
+                                <div>{review.clientName}</div>
+                                <div>{review.clientCompanyName}</div>
+                                <div>{review.clientRole}</div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
