a9a37cdeebc1a51f983b4f57e4d6a01b76259fad
aressss1 <codewithfaizi@gmail.com>
2024-02-08 10:24:33 +0500

    f-

--- components/featured-work-section.tsx
+++ components/featured-work-section.tsx
@@ -21,7 +21,7 @@ const FeaturedWork = () => {
                     )}>
                         Featured Work
                     </div>
-                    <Button size="lg" variant="special" className='bg-bg text-white rounded-[4rem]' >
+                    <Button size="lg" variant="special" className='rounded-[4rem]' >
                         See all work
                     </Button>
                 </div>
