541679ca0e0f942dc283fd001ed0c4a7226effdc
aressss1 <codewithfaizi@gmail.com>
2024-02-08 12:10:16 +0500

    f-

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         <div className='flex justify-center md:px-12 px-4' >
             <div
                 className={cn(
-                    "py-[2px] z-[99999] border rounded-[4rem] border-black flex justify-between items-center fixed lg:w-[79rem] md:w-[44rem] w-[19rem] top-[20px]",
+                    "py-[4px] z-[99999] border rounded-[4rem] border-black flex justify-between items-center fixed lg:w-[79rem] md:w-[44rem] w-[19rem] top-[20px]",
                     scrolled ? "bg-transparent backdrop-blur-[6px] backdrop-filter" : "bg-[url('/bg-2.jpg')]"
                 )} >
                 <div className="flex gap-2 p-2 justify-center items-center  " >
@@ -36,7 +36,7 @@ const Navbar = () => {
                         OnlyUp
                     </h1>
                 </div>
-                <Button variant="special" className='rounded-[4rem]'  >
+                <Button variant="special" className='rounded-[4rem] h-9 mr-2 '  >
                     Menu
                 </Button>
             </div>
