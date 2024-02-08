c45b561a0767d2f348c16b3cb47830f366ebed40
aressss1 <codewithfaizi@gmail.com>
2024-02-08 11:47:06 +0500

    f-

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -5,7 +5,7 @@ const AboutSection = () => {
                 About Us
             </div>
             <div className="flex leading-[1.2] lg:text-5xl md:text-3xl text-2xl lg:w-[57rem] md:w-[39rem]  text-black " >
-                We&apos;re a  London-based creative agency dedicated to transforming visions into artful reality, one client at a time.
+                We&apos;re a London-based creative agency dedicated to transforming visions into artful reality, one client at a time.
             </div>
             <div className="lg:text-2xl text-base lg:w-[47rem] md:w-[29rem]  " >
                 At our creative agency, we specialize in turning your unique ideas into captivating visual realities. With a passion for innovation, we bring your concepts to life, crafting designs that not only meet but exceed your expectations
