448372db016f481443c26594ac24b23d5b97ed73
aressss1 <codewithfaizi@gmail.com>
2024-02-12 10:00:43 +0500

    f-

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,23 +2,43 @@
 import Image from 'next/image';
 
 const Footer = () => {
-    return ( 
-        <div className="bg-white text-black flex justify-between  py-12 md:px-12 px-4" >
-            <div className="flex flex-col text-left gap-4" >
-                <div className="text-4xl" >
-                    ONLYUP
+    return (
+        <div className="bg-white text-black flex py-28 md:px-12 px-4" >
+            <div className='flex text-left gap-6' >
+                <div className='text-base' >
+                    Let&apos; Talk.
                 </div>
-                <div className="text-lg" >
-                    &copy; OnlyUp. All rights reserved.
+                <div className='text-5xl' >
+                    info@onlyup.com
                 </div>
             </div>
-            <div className="flex gap-4  " >
-                <Image src='/instagram.svg' alt='instagram' className='h-14 w-14'  />
-                <Image src='/twitter.svg' alt='twitter' className='h-14 w-14' />
-                <Image src='/linkedin.svg' alt='linkedin'  className='h-14 w-14' />
+            
+            <div className='flex flex-col text-left' >
+                <div className='text-base' >
+                    Job Applications
+                </div>
+                <div className='tetx-base opacity-90' >
+                    apply@onlyup.com
+                </div>
+            </div>
+
+            <div className='flex justify-between' >
+                <div className="flex flex-col text-left gap-12" >
+                    <div className="text-7xl" >
+                        ONLYUP
+                    </div>
+                    <div className="text-lg" >
+                        &copy; OnlyUp. All rights reserved.
+                    </div>
+                </div>
+                <div className="flex gap-8 opacity-80  " >
+                    <Image src='/instagram.svg' alt='instagram' className='h-6 w-6' />
+                    <Image src='/twitter.svg' alt='twitter' className='h-6 w-6' />
+                    <Image src='/linkedin.svg' alt='linkedin' className='h-6 w-6' />
+                </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Footer;
