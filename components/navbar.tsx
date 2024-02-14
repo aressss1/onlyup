aa115b98f3f970fb7dac72a7d307b686723de769
aressss1 <codewithfaizi@gmail.com>
2024-02-14 09:56:38 +0500

    f-

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,7 +14,7 @@ const menu = {
     open: {
         width: "480px",
         height: "650px",
-        top: "-25px",
+        top: "-26px",
         right: "0px",
         transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1] }
     },
