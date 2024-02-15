2b766afc4bd90f83af48630b4c591ca55f7feb8b
aressss1 <codewithfaizi@gmail.com>
2024-02-15 14:50:57 +0500

    f-

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -23,7 +23,13 @@ const HeroSection = () => {
 
     const animation = {
         initial: {y: "100%"},
-        enter: (i: number) => ({y: "0", transition: {duration: 0.75, ease: [0.33, 1, 0.68, 1],  delay: 0.075 * i}})
+        enter: (i: number) => ({
+            y: "0", 
+            transition: {
+                duration: 0.75, 
+                ease: [0.33, 1, 0.68, 1],  
+                delay: 0.095 * i
+            }})
       }
 
 
