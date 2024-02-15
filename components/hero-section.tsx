fc420b6f49b869106aee82bee330fb8ccc6c0d8d
aressss1 <codewithfaizi@gmail.com>
2024-02-15 14:19:16 +0500

    f-

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import localFont from 'next/font/local'
+import { motion } from 'framer-motion';
 
 import { cn } from "@/lib/utils";
 
@@ -10,8 +10,19 @@ const font = localFont({
     variable: '--font-gt-super',
 })
 
+const heroPhrases = [
+    "Turning Ideas into",
+    "Artful Realities."
+]
+
 
 const HeroSection = () => {
+    const animation = {
+        initial: {y: "100%"},
+        enter: (i: number) => ({y: "0", transition: {duration: 0.75, ease: [0.33, 1, 0.68, 1],  delay: 0.075 * i}})
+      }
+
+
     return (
         <div className="relative" >
             <div
@@ -19,12 +30,23 @@ const HeroSection = () => {
             />
             <div className="py-5 h-[90vh] md:h-screen flex flex-col relative z-[10] justify-between gap-28 items-center "  >
                 <div className="flex flex-col items-center gap-5 my-auto pt-36 " >
-                    <div className={cn(
-                        font.className,
-                        "lg:text-7xl md:text-5xl  text-2xl lg:leading-[5rem] lg:w-[730px] md:w-[565px] w-[263px] text-center "
+                    {heroPhrases.map((phrase , i) => (
+                        <div
+                            key={i}
+                            className={cn(
+                            font.className,
+                            "lg:text-7xl md:text-5xl  text-2xl lg:leading-[5rem] lg:w-[730px] md:w-[565px] w-[263px] text-center "
                     )}>
-                        Turning Ideas into Artful Realities.
+                        <motion.p 
+                            custom={i}  
+                            variants={animation} 
+                            initial="initial" 
+                            animate={"enter"} 
+                        >
+                            {phrase}
+                        </motion.p>
                     </div>
+                    ))}
                 </div>
 
                 <div>
