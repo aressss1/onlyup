25357026771f006f51af933c903816a8c8331427
aressss1 <codewithfaizi@gmail.com>
2024-02-15 10:06:39 +0500

    f-

--- components/ui/buttons/normal-button.tsx
+++ components/ui/buttons/normal-button.tsx
@@ -1,35 +1,46 @@
 'use client'
 
-import { useRouter } from "next/navigation";
+import { useRouter }  from "next/navigation";
 import { motion } from 'framer-motion';
 
 
 interface NormalButtonProps {
     label: string,
     href: string,
+    toggleMenu?: () => void
 }
 
 const NormalButton = ({
     label,
     href,
+    toggleMenu
 }: NormalButtonProps) => {
     const router = useRouter();
 
-    return (
-        <div className="normalButton" onClick={() => router.push(href)} >
-            <motion.div
-                className="slider"
+    return ( 
+        <div className="menuButton">
+        <motion.div
+            className="slider"
+            // animate={{ top: isActive ? "-100%" : "0%" }}
+            // transition={{ duration: 0.5, type: "tween", ease: [0.76, 0, 0.24, 1] }}
+        >
+            <div
+                className="el"
+                // onClick={() => { toggleMenu() }}
             >
-                <div
-                    className="el"
-                >
-                    <PerspectiveText label={label} />
-                </div>
-            </motion.div>
-        </div>
-    );
+                <PerspectiveText label={label} />
+            </div>
+            {/* <div
+                className="el"
+                onClick={() => { toggleMenu() }}
+            >
+                <PerspectiveText label={label} />
+            </div> */}
+        </motion.div>
+    </div>
+     );
 }
-
+ 
 export default NormalButton;
 
 function PerspectiveText({
@@ -39,7 +50,7 @@ function PerspectiveText({
 }) {
     return (
         <div className="perspectiveText">
-            <p className="capitalize" >{label}</p>
+            <p>{label}</p>
             <p>{label}</p>
         </div>
     )
