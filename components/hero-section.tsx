f00be03c7d01eec3b4af9928df22c3d925b1ba8c
aressss1 <codewithfaizi@gmail.com>
2024-02-15 15:00:08 +0500

    f-

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -60,8 +60,8 @@ const HeroSection = () => {
                 </div>
 
                 <motion.div
-                     initial={{ opacity: 0, x: -25 }}
-                     animate={{ opacity: 1, x: 0 }}
+                     initial={{ opacity: 0, y: 25 }}
+                     animate={{ opacity: 1, y: 0 }}
                      transition={{
                        duration: 0.2,
                        ease: "easeIn",
