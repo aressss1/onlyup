ef0058b9207aaae41cf44cad2c932e9f97b1ea94
aressss1 <codewithfaizi@gmail.com>
2024-02-15 14:54:31 +0500

    f-

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -28,7 +28,7 @@ const HeroSection = () => {
             transition: {
                 duration: 0.75, 
                 ease: [0.33, 1, 0.68, 1],  
-                delay: 0.095 * i
+                delay: 0.2 * i
             }})
       }
 
