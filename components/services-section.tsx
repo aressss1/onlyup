2b8cdb7eba921a2543d5511dcd30b9c270fe133b
aressss1 <codewithfaizi@gmail.com>
2024-02-15 10:58:30 +0500

    f-

--- components/services-section.tsx
+++ components/services-section.tsx
@@ -25,15 +25,15 @@ const ServicesSection = () => {
             <div className="text-sm font-normal opacity-80 uppercase " >
                 Services
             </div>
-            <div className="flex flex-col lg:gap-16 gap-12" >
+            <div className="flex flex-col lg:gap-14 gap-12" >
                 {services.map((service) => (
                     <div key={service.type} className="group" >
                         <div
-                            className="lg:text-5xl md:text-4xl text-xl text-black flex items-center gap-2" 
+                            className="lg:text-6xl md:text-4xl text-xl text-black group-hover:text-[#ef4242] flex items-center gap-2" 
                         >
                             {service.type}
                             <ArrowUpRight
-                                className="opacity-0 group-hover:opacity-100 lg:w-20 lg:h-20 md:w-16 w-8 h-12 stroke-1 " 
+                                className=" group-hover:text-[#ef4242] opacity-0 group-hover:opacity-100 lg:w-20 lg:h-20 md:w-16 w-8 h-12 stroke-[1.5] " 
                             />
                         </div>
                     </div>
