f66b4811e9ee7204f440f19f545d72172a1885ca
aressss1 <codewithfaizi@gmail.com>
2024-02-19 12:32:00 +0500

    f-

--- components/footer.tsx
+++ components/footer.tsx
@@ -75,9 +75,9 @@ const Footer = () => {
 
             <Separator className='bg-[#C4C4C4]' />
 
-            <div className=" flex justify-center flex-col md:flex-row  text-sm text-center py-4 opacity-70 " >
+            <div className="relative text-sm text-center py-4 opacity-70 " >
                 Copyright &copy; 2024 All rights reserved.
-                <div className='hidden md:flex self-end text-xs opacity-85 hover:opacity-100 ' >
+                <div className='hidden md:flex absolute top-3 right-2 text-xs opacity-85 hover:opacity-100 ' >
                     <Link href="/" >
                         Developed by Faizan Asghar
                     </Link>
